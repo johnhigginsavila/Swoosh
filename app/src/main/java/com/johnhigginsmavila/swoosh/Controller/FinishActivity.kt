d1dc3c6f3e8bce0949d450e8635d5857a61894a7
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-10-25 16:44:34 +0800

    Lesson 9

--- app/src/main/java/com/johnhigginsmavila/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/johnhigginsmavila/swoosh/Controller/FinishActivity.kt
@@ -12,6 +12,11 @@ class FinishActivity : AppCompatActivity() {
 
     lateinit var player : Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
@@ -19,6 +24,13 @@ class FinishActivity : AppCompatActivity() {
         player = intent.getParcelableExtra(EXTRA_PLAYER)
 
         searchLeagueTxt.text = "Looking for a ${player.league} ${player.skill} league near you..."
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
 }
--- app/src/main/java/com/johnhigginsmavila/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/johnhigginsmavila/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.johnhigginsmavila.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.johnhigginsmavila.swoosh.Model.Player
@@ -13,11 +14,23 @@ class SkillActivity : BaseActivity() {
 
     lateinit var player : Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
 
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
 
