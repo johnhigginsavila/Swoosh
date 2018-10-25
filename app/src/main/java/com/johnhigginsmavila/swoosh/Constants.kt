9d4e8310a4015d8116d58e9c412ae29a9e203d35
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-10-25 14:39:57 +0800

    Lesson 7

---
+++ app/src/main/java/com/johnhigginsmavila/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.johnhigginsmavila.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/johnhigginsmavila/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/johnhigginsmavila/swoosh/LeagueActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +17,36 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun leagueNextClicked (view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please Select a League", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+    fun onMenBtnClicked (view: View) {
+        womenLeagueBtn.isChecked = false
+        coEdLeagueBtn.isChecked = false
+
+        selectedLeague = "men"
+    }
+
+    fun onWomenBtnClicked (view: View) {
+        menLeagueBtn.isChecked = false
+        coEdLeagueBtn.isChecked = false
+
+        selectedLeague = "women"
+    }
+
+    fun onCoEdBtnClicked (view: View) {
+        menLeagueBtn.isChecked = false
+        womenLeagueBtn.isChecked = false
+
+        selectedLeague = "coed"
+    }
+
+
 }
